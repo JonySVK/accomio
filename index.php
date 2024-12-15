6183da9d8e26069d90956ba130600ee312d3ecef
JonySVK <153192660+JonySVK@users.noreply.github.com>
2024-12-15 14:33:01 +0100

    Backup 15/12/2024

--- index.php
+++ index.php
@@ -15,33 +15,74 @@ to-dos:
             <div class="title">accomio</div>
             <nav class="headerbtns">
                 <a onclick="langbox()" class="aimg"><div class="headerdiv" id="hi1">
-                    <span class="headertext" id="ht1">Zmeniť jazyk</span>
-                    <img src="styles/icons/language.svg" class="headerimgs">
+                    <abbr class="headertext" id="ht1" title="Zmeniť jazyk"><img src="styles/icons/language.svg" class="headerimgs"></abbr>
                     <div id="hi1style" style="display:inline;"></div>
                 </div></a>
                 <a href="help" class="aimg"><div class="headerdiv" id="hi2">
-                    <span class="headertext" id="ht2">Zákaznícka podpora</span>
-                    <img src="styles/icons/help.svg" class="headerimgs">
+                    <abbr class="headertext" id="ht2" title="Zákaznícka podpora"><img src="styles/icons/help.svg" class="headerimgs"></abbr>
                 </div></a>
-                <a href="account" class="aimg"><div class="headerdiv" id="hi3">
-                    <span class="headertext" id="ht3">Prihlásiť sa/Registrovať sa</span>
-                    <img src="styles/icons/account.svg" class="headerimgs">
+                <a onclick="loginbox()" class="aimg"><div class="headerdiv" id="hi3">
+                    <abbr class="headertext" id="ht3" title="Prihlásiť sa/Registrovať sa"><img src="styles/icons/account.svg" class="headerimgs"></abbr>
+                    <div id="loginstyle" style="display:inline;"></div>
                 </div></a>
             </nav>
             <div id="lang-box">
                 <span class="langtext">Choose your language:</span><br>
-                <img src="styles/languages/english.svg" id="lang-en" class="langimg">
-                <img src="styles/languages/german.svg" id="lang-de" class="langimg">
-                <img src="styles/languages/slovak.svg" id="lang-sk" class="langimg">
-                <img src="styles/languages/czech.svg" id="lang-cz" class="langimg">
+                <abbr title="English"><img src="styles/languages/english.svg" id="lang-en" class="langimg"></abbr>
+                <abbr title="Deutsch"><img src="styles/languages/german.svg" id="lang-de" class="langimg"></abbr>
+                <abbr title="Slovensky"><img src="styles/languages/slovak.svg" id="lang-sk" class="langimg"></abbr>
+                <abbr title="Česky"><img src="styles/languages/czech.svg" id="lang-cz" class="langimg"></abbr>
+            </div>
+            <div id="login-box">
+                <span class="langtext">Prihláste sa:</span><br>
+                <form id="login">
+                    <label for="email">Email:</label>
+                    <input type="text" id="email" name="email"><br>
+                    <label for="password">Heslo:</label>
+                    <input type="password" id="password" name="password"><br>
+                    <input type="submit" value="Prihlásiť sa">
+                </form><br>
+                <span class="langtext">Nemáš ešte účet? <a href="register">Zaregistruj sa</a></span>
             </div>
         </header>
         <div id="search" style="color:white;">
             <div class="welcometext">Kam to dnes bude?</div>
             <form id="searchform">
-                <input type="text" id="place" name="place">
-                <input type="date" id="datefrom" name="datefrom" oninput="dateto.min = this.value">
-                <input type="date" id="dateto" name="dateto" oninput="datefrom.max = this.value">
+                <div id="placediv" class="formdiv">
+                <label for="place">Kam cestujete?</label><br>
+                <input list="placelist" class="searchinput" id="place" name="place" required>
+                    <datalist id="placelist">
+                        <div id="placelistdiv" class="formdiv">
+                            <!-- delete after install php -->    
+                            <option value="Bratislava">
+                            <option value="Viedeň">
+                            <option value="Budapešť">
+                            <option value="Berlín">
+                            <option value="Londýn">
+                            <!-- end -->
+                        </div>
+                    </datalist>
+                </div>
+                <div id="datefromdiv" class="formdiv">
+                    <label for="datefrom">Príchod</label><br>
+                    <input type="date" class="searchinput" id="datefrom" name="datefrom" oninput="dateto.min = this.value" min="<?php echo date("Y-m-d"); ?>" required>
+                </div>
+                <div id="datetodiv" class="formdiv">
+                    <label for="dateto">Odchod</label><br>
+                    <input type="date" class="searchinput" id="dateto" name="dateto" oninput="datefrom.max = this.value" min="<?php echo date("Y-m-d"); ?>" required>
+                </div>
+                <div id="adultsdiv" class="formdiv">
+                    <label for="adults">Počet dospelých</label><br>
+                    <input type="number" class="searchinput" id="adults" name="adults" value="1" min="1" max="20" style="width: 6.5vw; text-align: center;" required>
+                </div>
+                <div id="kidsdiv" class="formdiv">
+                    <label for="kids">Počet detí</label><br>
+                    <input type="number" class="searchinput" id="kids" name="kids" value="0" min="0" max="10" style="width: 6.5vw; text-align: center;" required>
+                </div>
+                <div id="submitdiv" class="formdiv">
+                    <label></label><br>
+                    <input type="image" src="styles/icons/search_white.svg" id="submitimage" onmouseover="document.querySelector('#submitimage').src='styles/icons/search_black.svg'" onmouseout="document.querySelector('#submitimage').src='styles/icons/search_white.svg'">
+                </div>
             </form>
         </div>
     </body>
--- scripts/basic.js
+++ scripts/basic.js
@@ -9,3 +9,39 @@ function langbox() {
         langboxvisible = false
     }
 }
+
+var loginboxvisible = false
+function loginbox() {
+    var loginstyle = document.querySelector("#hi1style")
+    if (loginboxvisible === false) {
+        loginstyle.innerHTML = "<style>#login-box {visibility: visible; height: auto; width: 20vw;}</style>"
+        loginboxvisible = true
+    } else {
+        loginstyle.innerHTML = "<style>#login-box {visibility: hidden; height: 0; width: 0;}</style>"
+        loginboxvisible = false
+    }
+}
+
+var placeholder
+var cities = ["Bratislava", "Viedeň", "Berlín"];
+var currentCityIndex = 0;
+var charIndex = 0;
+var isDeleting = false;
+setInterval(function () {
+    const currentCity = cities[currentCityIndex]
+
+    if (!isDeleting) {
+        placeholder = currentCity.slice(0, charIndex++)
+        if (charIndex > currentCity.length) {
+            isDeleting = true
+        }
+    } else {
+        placeholder = currentCity.slice(0, charIndex--)
+        if (charIndex < 0) {
+            isDeleting = false
+            currentCityIndex = (currentCityIndex + 1) % cities.length
+        }
+    }
+
+    document.querySelector('#place').placeholder = placeholder
+}, 1000);
